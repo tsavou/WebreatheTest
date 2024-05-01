d3a29cda97cf723893b10dded61bb32cb7d3b3c7
Theo S <theosavourat@gmail.com>
2024-05-01 02:47:24 +0200

    📈 Graphique d'évolution des mesures

--- resources/views/modules/show.blade.php
+++ resources/views/modules/show.blade.php
@@ -2,18 +2,54 @@
 
 
 @section('content')
-    <div>
-
-        <h1>{{ $module->name }}</h1>
-
-        <p>{{ $module->details }}</p>
-
-        <p>Date d'installation : {{ $module->installation_date }}</p>
-
-        <p>{{ $nbMesures }} données envoyées</p>
+    <a href="{{ route('modules.index') }}" class="btn btn-dark">
+        Retour </a>
 
 
-            <span class="badge {{ $module->status ? 'bg-success' : 'bg-danger' }}">{{ $module->status ? 'Actif' : 'Inactif' }}</span> depuis {{ $duration->format('%H h %i min %s s') }}
+
+    <div class="container bg-body-tertiary rounded">
+
+        <div class="text-center mb-5">
+            <h1 class="fw-bold text-uppercase ">{{ $module->name }}</h1>
+            <p>installé le : {{ date('d/m/Y', strtotime($module->installation_date)) }}</p>
+            <p>{{ $module->details }}</p>
+        </div>
+
+        <div class="d-flex justify-content-center align-items-center gap-5 my-5 ">
+            <div class="card bg-body-tertiary text-white" data-bs-theme="dark">
+                <div class="card-body text-center">
+
+                    <p>
+                        <span
+                            class="badge {{ $module->status ? 'bg-success' : 'bg-danger' }}">{{ $module->status ? 'Actif' : 'Inactif' }}
+                        </span>
+                        depuis :
+                    </p>
+                    <h3>
+                        {{ $duration->format('%H h %i min %s s') }}
+                    </h3>
+                </div>
+            </div>
+
+            <div class="card bg-body-tertiary text-white" data-bs-theme="dark">
+                <div class="card-body text-center">
+
+                    <p>Nombre de données envoyées :</p>
+                    <h3>
+                        {{ $nbMesures }}
+                    </h3>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="w-75 mx-auto my-5">
+
+            <canvas id="myChart" aria-label="chart" role="img">
+            </canvas>
+
+        </div>
+
 
 
         <table class="table table-striped table-dark">
@@ -33,4 +69,47 @@
 
 
     </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
+
+    <script>
+        const labels = [];
+        const data = [];
+
+
+        @foreach ($mesures as $mesure)
+            labels.push('{{ date('H:i:s', strtotime($mesure->date_mesure)) }}');
+            data.push({{ $mesure->valeur_mesuree }});
+        @endforeach
+
+
+
+
+        const canvas = document.getElementById('myChart');
+
+
+
+        const myChart = new Chart(canvas, {
+            type: 'line',
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            },
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '{{ $module->type_mesure }}',
+                    data: data,
+                    borderWidth: 2,
+                    borderColor: 'rgb(75, 192, 192)',
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    tension: 0.4
+
+                }]
+            }
+        })
+    </script>
 @endSection
