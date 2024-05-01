13cb91a2dca26d2369393d12d3d64b98a723c59f
Theo S <theosavourat@gmail.com>
2024-05-01 03:25:38 +0200

    🎨simple page d'accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
-    <h1>Welcome</h1>
+    <h1 class="text-center my-5">Bienvenue sur notre système de monitoring IoT !</h1>
+
+    <p class="text-center my-5">Notre système de monitoring IoT est conçu pour vous permettre de surveiller en temps réel l'état de fonctionnement de vos modules connectés à internet </p>
+
+    <p>Enregistrez facilement de <a href="{{ route('modules.create') }}" class="btn btn-dark">nouveaux modules IoT</a> pour les intégrer à votre système de monitoring.</p>
+
+    <p>Pour voir les modules enregistrés, <a href="{{ route('modules.index') }}" class="btn btn-dark">cliquez ici</a></p>
+
+    <p> Pour chaque module, visualisez instantanément l'état de fonctionnement de vos modules, y compris les valeurs mesurées en temps réel, la durée de fonctionnement et le nombre de données envoyées. Analysez l'historique des données de vos modules à l'aide de graphiques interactifs pour détecter les tendances et les variations.</p>
 
 @endsection
